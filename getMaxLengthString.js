764288ebed72779c7f3bbb5c83db8d7e7682fb4d
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-09-07 14:49:13 +0700

    test to get longest word

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,26 +1,13 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  // let longest
-  // for (let item of arrayOfString) {
-  //   if (item.length > longest.length) {
-  //       longest = item
-  //   }
-  //   else {
-  //     console.log(undefined)
-  //   }
-  // }
-  // console.log(longest)
   let longest
   for (let item of arrayOfString) {
     if (item.length > longest.length) {
-      longest = item
+      longest = length
     }
-    return longest
   }
-  if (arrayOfString.includes(undefined) | arrayOfString.includes(null)) {
-    return undefined
-  }
+  return longest
 }
 
 module.exports = getMaxLengthString
