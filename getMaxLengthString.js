9248a5ff812cbd5a4458b8726d261a21ff8acb84
TanasitISTG <tanasit.istg@mail.kmutt.ac.th>
2022-09-07 14:30:02 +0700

    test while im in classssssssssssssss

--- getMaxLengthString.js
+++ getMaxLengthString.js
@@ -1,6 +1,16 @@
 const { template } = require('@babel/core')
 
 function getMaxLengthString(arrayOfString) {
-  //code here
+  // let longest
+  // for (let item of arrayOfString) {
+  //   if (item.length > longest.length) {
+  //       longest = item
+  //   }
+  //   else {
+  //     console.log(undefined)
+  //   }
+  // }
+  // console.log(longest)
 }
+
 module.exports = getMaxLengthString
